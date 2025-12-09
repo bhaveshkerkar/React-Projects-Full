b6298f3602ace712d1147e6e604a6516650a4fda
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-09 17:27:29 +0530

    Initial commit with all React projects

---
+++ 8-bharat-version-two/src/components/ClockSlogan.jsx
@@ -0,0 +1,7 @@
+let ClockSlogan = () => {
+  return (
+    <p className="lead">This is the Clock That Shows The Bharat At All Times</p>
+  );
+};
+
+export default ClockSlogan;
---
+++ 8-bharat-version-two/src/components/CurrentTime.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+
+let CurrentTime = () => {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+
+  }, []);
+
+  return (
+    <p className="lead">
+      This is The Current Time : {time.toLocaleDateString()} -{" "}
+      {time.toLocaleTimeString()}
+    </p>
+  );
+};
+
+export default CurrentTime;
