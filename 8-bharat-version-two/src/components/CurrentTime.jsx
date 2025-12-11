4dab48a590ce0a4c0d9cc42d26f8ef8c749dbfcb
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-11 23:40:08 +0530

    edit file

--- 8-bharat-version-two/src/components/CurrentTime.jsx
+++ 8-bharat-version-two/src/components/CurrentTime.jsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from "react";
 
 let CurrentTime = () => {
   const [time, setTime] = useState(new Date());
+
   useEffect(() => {
     const intervalId = setInterval(() => {
       setTime(new Date());
     }, 2000);
 
-    return () => clearInterval(intervalId);
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
 
   return (
