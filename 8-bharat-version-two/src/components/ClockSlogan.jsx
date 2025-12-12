71edd4071e6b377a5a41f78a1f5e25669356b852
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-12-12 23:48:41 +0530

    edit file

--- 8-bharat-version-two/src/components/ClockSlogan.jsx
+++ 8-bharat-version-two/src/components/ClockSlogan.jsx
@@ -1,7 +1,5 @@
 let ClockSlogan = () => {
-  return (
-    <p className="lead">This Shows the time of all countries in Bharat </p>
-  );
+  return <p className="lead">This Shows the time of Bharat </p>;
 };
 
 export default ClockSlogan;
