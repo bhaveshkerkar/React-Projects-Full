c444ce38594cbee92158802a3b23da227aeb7c1a
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2026-01-24 15:46:50 +0530

    changs tha path of bag

---
+++ 9-Myntra-Clone/3-myntra-react-clone/src/routes/Bags.jsx
@@ -0,0 +1,12 @@
+const Bags = () => {
+  return (
+    <main>
+      <div className="bag-page">
+        <div className="bag-items-container"></div>
+        <div className="bag-summary"></div>
+      </div>
+    </main>
+  );
+};
+
+export default Bags;
